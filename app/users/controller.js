f71e7cc327a04a4ca807febc2cb85a4ab4f6a54d
ZECAMINHAUM <ls4388387@gmail.com>
2019-05-14 02:11:13 -0300

    validation for data in request and if user exists

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,16 +1,41 @@
 const User = require("./model");
+const validator = require('./validator');
+const isEmpty = require('../../is-empty');
+
 
 exports.register = (req, res) => {
+
+	//verify errors in data
+	const { errors ,isValid } = validator.validateRegister(req.body);
+	
+	//if have some error return for user
+	if(!isValid){
+		return res.status(401).json({success: false, errors});
+	}
 	const { name, email, password } = req.body;
 
-	const newUser = new User({
-		name,
-		email,
-		password
-	});
+	//search for current email in database
+	User.findOne({email}, async (error, data) => {
+		//if have some error in request
+		if(error) { return res.status(500).json({ success: false, error} )};
+		//verify if response find some user, and if not, voilá 
+		if(!isEmpty(data)){
+			return res.status(401).json({success: false, errors: 'Email is already been taken'});
+		}else{ 
+			//creating a new User object with data
+			const newUser = new User({
+				name,
+				email,
+				password
+			});
 
-	newUser
-		.save()
-		.then(user => res.status(201).json({ success: true, user }))
-		.catch(err => res.status(500).json({ success: false, err }));
+			//trying to save data in database
+			newUser
+				.save()
+				.then(user => res.status(201).json({ success: true, user }))
+				.catch(err => res.status(500).json({ success: false, err }));
+		}
+	})
+
+
 };
